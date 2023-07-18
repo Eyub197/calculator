e2a3491110dae53aca44e0eb27376b0843bbbc27
Eyub197 <eyubmehmed122@gmail.com>
2023-07-18 10:27:29 +0300

    Made an operate function for the claculator to make the ui easier to code

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ let subtract = (number1 , number2) => {return number1 - number2}
 let multiply = (number1 , number2) => {return number1 * number2}
 
 let divide = () => (number1 , number2) => {return number1 / number2}
+
+let operate = (oper, num1, num2) =>{
+    switch(oper){
+        case "+" :
+            add(num1, num2)
+            break
+        case "-" :
+            subtract(num1, num2)
+            break
+        case "*" :
+            multiply(num1, num2)
+            break
+        case "/" :
+            divide(num1, num2)
+            break
+        default : 
+            console.log("added an valid operation")
+    }
+    
+}
