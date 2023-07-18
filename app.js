d26388f7a625d623ce1271ea680c580bc0c1390f
Eyub197 <eyubmehmed122@gmail.com>
2023-07-18 10:18:00 +0300

    added variables to store the data for future display showing

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let number1 = 0
+let operation = "+"
+let number2 = 0
+
 let add = (number1, number2) => {return number1 + number2 }
 
 let subtract = (number1 , number2) => {return number1 - number2}
