94267c39922a1ab4afb4e4254176eaaf7aaf244c
Eyub197 <eyubmehmed122@gmail.com>
2023-07-18 10:16:54 +0300

    made the basic functions

---
+++ app.js
@@ -0,0 +1,7 @@
+let add = (number1, number2) => {return number1 + number2 }
+
+let subtract = (number1 , number2) => {return number1 - number2}
+
+let multiply = (number1 , number2) => {return number1 * number2}
+
+let divide = () => (number1 , number2) => {return number1 / number2}
